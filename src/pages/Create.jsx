0680f6e3b0bf1f13b683e1c47f472b9a8852c17f
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-12-17 22:22:35 +0800

    Icon and install @material-ui/icons

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -3,6 +3,7 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Container from '@material-ui/core/Container'
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
 
 export default class Create extends Component {
     render() {
@@ -47,6 +48,23 @@ export default class Create extends Component {
                 <br />
                 <br />
                 <Button onClick={() => console.log('You clicked me')}type="submit" color="secondary" variant="contained">Submit</Button>
+                <br />
+                <br />
+                {/* Icon */}
+                <AcUnitOutlinedIcon />
+                <AcUnitOutlinedIcon color="secondary" fontSize="large"/>
+                <AcUnitOutlinedIcon color="secondary" fontSize="small"/>
+                <AcUnitOutlinedIcon color="action" fontSize="small"/>
+                <AcUnitOutlinedIcon color="error" fontSize="small"/>
+                <AcUnitOutlinedIcon color="disabled" fontSize="small"/>
+                <br />
+                <br />
+                <Button 
+                    type="submit" 
+                    color="secondary" 
+                    variant="contained"
+                    startIcon={<AcUnitOutlinedIcon />}
+                    endIcon={<AcUnitOutlinedIcon />}>Submit</Button>
             </Container>
         )
     }
