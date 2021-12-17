60e192617d23b5f6ae1e3d5d1870783d1be4f42b
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-12-17 20:08:49 +0800

    button, button group, container

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,13 @@
 import { Component } from 'react'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Container from '@material-ui/core/Container'
 
 export default class Create extends Component {
     render() {
         return (
-            <div>
+            <Container>
                 <Typography variant="h1" color="primary" align="center">
                     Create a new Note
                 </Typography>
@@ -22,7 +25,29 @@ export default class Create extends Component {
                 <Typography variant="h6" component="h2">
                     h2
                 </Typography>
-            </div>
+                <br />
+
+                <Button type="submit">Submit</Button>
+                <Button type="submit" color="secondary" variant="contained">Submit</Button>
+                <Button type="submit" color="secondary" variant="outlined">Submit</Button>
+                <br />
+                <br />
+                <ButtonGroup color="secondary">
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
+                <br />
+                <br />
+                <ButtonGroup variant="contained" color="secondary">
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
+                <br />
+                <br />
+                <Button onClick={() => console.log('You clicked me')}type="submit" color="secondary" variant="contained">Submit</Button>
+            </Container>
         )
     }
 }
