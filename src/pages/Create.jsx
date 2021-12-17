732fe3220b7a5e554526f8b7d47e2f367b577e87
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-12-17 19:17:13 +0800

    Typography

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,28 @@
 import { Component } from 'react'
+import Typography from '@material-ui/core/Typography'
 
 export default class Create extends Component {
     render() {
         return (
-            <h2>Create Page</h2>
+            <div>
+                <Typography variant="h1" color="primary" align="center">
+                    Create a new Note
+                </Typography>
+                <Typography noWrap color="secondary">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
+        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                </Typography>
+                <Typography variant="h6">
+                    h6
+                </Typography>
+                <Typography variant="h6" component="h2" gutterBottom>
+                    h2
+                </Typography>
+                <Typography variant="h6" component="h2">
+                    h2
+                </Typography>
+            </div>
         )
     }
 }
