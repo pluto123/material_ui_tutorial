4c3b36e3441cca7191ac93c8fec9d87d9862efdc
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-12-18 08:04:28 +0800

    add functional component usage

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,12 +1,11 @@
 import { Component } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Container from '@material-ui/core/Container'
-import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import { withStyles } from "@material-ui/core/styles"
 
-const useStyles = (theme) => ({
+const useStyles = () => ({
     btn: {
         fontSize: 60,
         backgroundColor: 'violet',
@@ -25,65 +24,26 @@ class Create extends Component {
         const { classes } = this.props;
         return (
             <Container>
-                <Typography 
-                    className={classes.title}
-                    variant="h1" color="primary" align="center">
-                    Create a new Note
-                </Typography>
-                <Typography noWrap color="secondary">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-                </Typography>
-                <Typography variant="h6">
-                    h6
-                </Typography>
-                <Typography variant="h6" component="h2" gutterBottom>
-                    h2
-                </Typography>
-                <Typography variant="h6" component="h2">
-                    h2
-                </Typography>
-                <br />
-
-                <Button type="submit">Submit</Button>
-                <Button type="submit" color="secondary" variant="contained">Submit</Button>
-                <Button type="submit" color="secondary" variant="outlined">Submit</Button>
-                <br />
-                <br />
-                <ButtonGroup color="secondary">
-                    <Button>One</Button>
-                    <Button>Two</Button>
-                    <Button>Three</Button>
-                </ButtonGroup>
-                <br />
-                <br />
-                <ButtonGroup variant="contained" color="secondary">
-                    <Button>One</Button>
-                    <Button>Two</Button>
-                    <Button>Three</Button>
-                </ButtonGroup>
-                <br />
-                <br />
-                <Button onClick={() => console.log('You clicked me')}type="submit" color="secondary" variant="contained">Submit</Button>
-                <br />
-                <br />
-                {/* Icon */}
-                <AcUnitOutlinedIcon />
-                <AcUnitOutlinedIcon color="secondary" fontSize="large"/>
-                <AcUnitOutlinedIcon color="secondary" fontSize="small"/>
-                <AcUnitOutlinedIcon color="action" fontSize="small"/>
-                <AcUnitOutlinedIcon color="error" fontSize="small"/>
-                <AcUnitOutlinedIcon color="disabled" fontSize="small"/>
-                <br />
-                <br />
-                <Button className={classes.btn}
-                    type="submit" 
-                    color="secondary" 
-                    variant="contained"
-                    startIcon={<AcUnitOutlinedIcon />}
-                    endIcon={<AcUnitOutlinedIcon />}>Submit</Button>
-            </Container>
+            <Typography
+                className={classes.title} 
+                variant="h6" 
+                color="textSecondary" 
+                component="h2" 
+                gutterBottom
+            >
+                Create a new note
+            </Typography>
+            <Button 
+                className={classes.btn}
+                onClick={() => console.log('You clicked me')}
+                type="sumbit"
+                color="secondary"
+                variant="contained"
+                endIcon={<KeyboardArrowRightIcon />}
+            >
+                Submit
+            </Button>
+        </Container>
         )
     }
 }
--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,9 +1,48 @@
-import { Component } from 'react'
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from "@material-ui/core/styles"
 
-export default class Create extends Component {
-    render() {
-        return (
-            <h2>Note Page</h2>
-        )
+const useStyles = makeStyles({
+    btn: {
+        fontSize: 60,
+        backgroundColor: 'violet',
+        '&:hover': {
+            backgroundColor: 'blue'
+        }
+    },
+    title: {
+        textDecoration: 'underline',
+        marginBottom: 20
     }
+})
+
+export default function Create() { 
+    const classes = useStyles()
+
+    return (
+        <Container>
+            <Typography
+                className={classes.title} 
+                variant="h6" 
+                color="textSecondary" 
+                component="h2" 
+                gutterBottom
+            >
+                Show a Note
+            </Typography>
+            <Button 
+                className={classes.btn}
+                onClick={() => console.log('You clicked me')}
+                type="sumbit"
+                color="secondary"
+                variant="contained"
+                endIcon={<KeyboardArrowRightIcon />}
+            >
+                Submit
+            </Button>
+        </Container>
+    )
 }
