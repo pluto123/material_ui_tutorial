697299b62d39fc81ae5bba00db2dcfbab6a0a16a
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-12-18 21:16:45 +0800

    自定義 Theme 顏色

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,31 @@ import { Component } from 'react'
 import { Route, Switch, Link } from 'react-router-dom'
 import Create from './pages/Create'
 import Note from './pages/Note'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+import { purple } from '@material-ui/core/colors'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FEFEFE'
+    },
+    secondary: purple
+  }
+})
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Link to='/create'>Create</Link><br />
-        <Link to='/note'>Note</Link>
-        <Switch>
-            <Route path='/create' component={Create} />
-            <Route path='/note' component={Note} />
-        </Switch>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div>
+          <Link to='/create'>Create</Link><br />
+          <Link to='/note'>Note</Link>
+          <Switch>
+              <Route path='/create' component={Create} />
+              <Route path='/note' component={Note} />
+          </Switch>
+        </div>
+      </ThemeProvider>
     )
   }
 }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -3,29 +3,12 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import { withStyles } from "@material-ui/core/styles"
-
-const useStyles = () => ({
-    btn: {
-        fontSize: 60,
-        backgroundColor: 'violet',
-        '&:hover': {
-            backgroundColor: 'blue'
-        }
-    },
-    title: {
-        textDecoration: 'underline',
-        marginBottom: 20
-    }
-})
 
 class Create extends Component {
     render() {
-        const { classes } = this.props;
         return (
             <Container>
             <Typography
-                className={classes.title} 
                 variant="h6" 
                 color="textSecondary" 
                 component="h2" 
@@ -33,9 +16,18 @@ class Create extends Component {
             >
                 Create a new note
             </Typography>
-            <Button 
-                className={classes.btn}
+            <Button
                 onClick={() => console.log('You clicked me')}
+                type="sumbit"
+                color="primary"
+                variant="contained"
+                endIcon={<KeyboardArrowRightIcon />}
+            >
+                Submit
+            </Button>
+            <br />
+            <br />
+            <Button
                 type="sumbit"
                 color="secondary"
                 variant="contained"
@@ -48,4 +40,4 @@ class Create extends Component {
     }
 }
 
-export default withStyles(useStyles) (Create)
+export default Create
