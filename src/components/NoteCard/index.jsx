1ad6fae0c7fd2226c05fe69c9d778c9e93dbdba9
Steven Wu <stevenwu@mirle.com.tw>
2021-12-28 10:30:11 +0800

    可以有紅色外框，缺少動態設定外框顏色

--- src/components/NoteCard/index.jsx
+++ src/components/NoteCard/index.jsx
@@ -4,14 +4,22 @@ import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import { DeleteOutlined } from '@material-ui/icons'
 import { IconButton, Typography} from '@material-ui/core'
+import { withStyles } from "@material-ui/core/styles"
 
-export default class NoteCard extends Component {
+const useStyles = () => ({
+    test: {
+        border: '1px red solid'
+    }
+})
+
+class NoteCard extends Component {
     render() {
         const {id, title, category, details} = this.props
+        const { classes } = this.props
 
         return (
             <div>
-                <Card elevation={1}>
+                <Card elevation={1} className={classes.test}>
                     <CardHeader
                         action={
                             <IconButton onClick={() => this.props.handleDelete(id)}>
@@ -31,3 +39,5 @@ export default class NoteCard extends Component {
         )
     }
 }
+
+export default withStyles(useStyles) (NoteCard)
