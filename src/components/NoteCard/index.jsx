0b8f7142c32f4219ab3d46e0dd1364942822130c
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-12-28 20:31:05 +0800

    Avatar

--- src/components/NoteCard/index.jsx
+++ src/components/NoteCard/index.jsx
@@ -6,12 +6,42 @@ import { DeleteOutlined } from '@material-ui/icons'
 import { IconButton, Typography} from '@material-ui/core'
 import Avatar from '@material-ui/core/Avatar'
 import { withStyles } from "@material-ui/core/styles"
+import { blue, green, pink, yellow } from '@material-ui/core/colors'
 
 const useStyles = () => ({
     red_border: {
         border: '1px red solid'
+    },
+    avatar_work: {
+        backgroundColor: yellow[700]
+    },
+    avatar_money: {
+        backgroundColor: green
+    },
+    avatar_todos: {
+        backgroundColor: pink[500]
+    },
+    avatar_others: {
+        backgroundColor: blue[500]
     }
 })
+
+const checkCategory = (category, classes) => (
+    (category === 'work')?classes.red_border:null    
+)
+
+const getAvatar = (category, classes) => {
+    if (category === 'work') {
+        return classes.avatar_work
+    }
+    if (category === 'money') {
+        return classes.avatar_money
+    }
+    if (category === 'todos') {
+        return classes.avatar_todos
+    }
+    return classes.avatar_others
+}
 
 class NoteCard extends Component {
     render() {
@@ -19,10 +49,10 @@ class NoteCard extends Component {
 
         return (
             <div>
-                <Card elevation={1} className={(category === 'work')?classes.red_border:null}>
+                <Card elevation={1} className={checkCategory(category, classes)}>
                     <CardHeader
                         avatar={
-                            <Avatar>
+                            <Avatar className={getAvatar(category, classes)}>
                                 {category[0].toUpperCase()}
                             </Avatar>
                         }
